bbfc6645248ca466761244daffea1ed88d70b389
Omkar <omkarr24@gmail.com>
2020-04-20 12:59:52 +0530

    First Demo On Git Hub

---
+++ src/com/demo/git/Test.java
@@ -0,0 +1,15 @@
+package com.demo.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Omkar");
+		String Name = "Omkar";
+		if(Name.equals("Omkar")) 
+		{
+			System.out.println("Its a Name");
+		}
+	}
+
+}
